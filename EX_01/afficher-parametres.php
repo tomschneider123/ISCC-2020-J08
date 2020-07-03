c71991cab46253338b38e0ec4357264af34b9a92
Tom Schneider <tom.schneider@iseg.fr>
2020-07-03 16:45:29 +0200

    j'ai encore un peu de temps

---
+++ EX_01/afficher-parametres.php
@@ -0,0 +1,12 @@
+<?php
+
+foreach ($_GET as $key => $value)
+{
+echo "<ul>";
+echo "<li>";
+echo " cle: " . $key . " valeur: " .$value ;
+echo "</ul>";
+}
+
+
+?>
---
+++ EX_01/mini-site-routing.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf8" />
+        <link rel="stylesheet" href="hello-world-css.css" />
+        <title>mini-site-routing</title>
+    </html>
+    <body>
+        <h1>Hello World !</h1>
+    </body>
+</html>
